0230d247705fe3da3ce04e9c6adf963eaebb6dbb
Evgeniy <ekukonen@gmail.com>
2021-03-25 23:38:54 +0300

    change location

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -3,7 +3,7 @@ const db = require('../db')
 class UserController {
     async createUser(req, res) {
         const {login, email, password} = req.body;
-        console.log({login, email, password})
+        console.log(req.body)
         const role = "user"
         const newUser = await db.query(`INSERT INTO public."Users" (login, email, password, role) values ($1, $2, $3, $4)`, [login, email, password, role])
         res.json("ok")
